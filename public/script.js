b3d028e5d3957b647398979979e71424213d7fc7
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-09-23 03:56:51 +0100

    add element dragg in js

---
+++ public/script.js
@@ -0,0 +1,25 @@
+const draggableButton = document.getElementById('draggable-button');
+const priceDisplay = document.getElementById('price');
+let isDragging = false;
+
+// Add event listeners to start and stop dragging
+draggableButton.addEventListener('mousedown', () => {
+  isDragging = true;
+  draggableButton.style.transition = 'none';
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+  draggableButton.style.transition = 'all 0.3s ease';
+});
+
+// Listen for mousemove events and update the price accordingly
+document.addEventListener('mousemove', (e) => {
+  if (isDragging) {
+    const xPosition = e.clientX - draggableButton.offsetWidth / 2;
+    const price = Math.max(10, Math.min(100, Math.round(xPosition / 4)));
+    
+    draggableButton.style.left = xPosition + 'px';
+    priceDisplay.textContent = `$${price.toFixed(2)}` + " ";
+  }
+});
